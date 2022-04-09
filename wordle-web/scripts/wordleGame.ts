fcd5fe2a8ecb8cd8e5cd2b168e1585c942df154b
Grant Erickson <grant@granterickson.com>
2022-04-09 07:44:03 -0700

    Added WordsService and testing

--- wordle-web/scripts/wordleGame.ts
+++ wordle-web/scripts/wordleGame.ts
@@ -1,1 +1,17 @@
-export class WordleGame {}
+import { Word } from "./word";
+import { WordsService } from "./wordsService";
+
+export class WordleGame {
+	constructor() {
+		this.currentWord = new Word();
+		this.words.push(this.currentWord);
+		this.word = WordsService.getRandomWord();
+	}
+
+	private word: string
+	words: Word[] = []
+	currentWord: Word
+
+
+
+}
