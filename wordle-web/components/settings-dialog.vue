445521476bedf9828bb9c0bf1695fc0154a9ba45
Meghan <Meg.Woodford@IntelliTect.com>
2022-04-19 15:45:27 -0700

    Adding purple colors

--- wordle-web/components/settings-dialog.vue
+++ wordle-web/components/settings-dialog.vue
@@ -27,6 +27,21 @@
                 </v-list-item-group>
               </v-list>
             </v-menu>
+
+            <v-menu offset-y>
+              <template #activator="{ on, attrs }">
+                <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                  Pick A Color
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item-group>
+                  <v-list-item @click="purpleTheme">
+                    <v-list-item-title> Purple </v-list-item-title>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-menu>
           </v-card-text>
         </v-container>
       </v-card>
@@ -36,6 +51,7 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { colors } from 'vuetify/lib'
 
 @Component({})
 export default class SettingsDialog extends Vue {
@@ -51,6 +67,21 @@ export default class SettingsDialog extends Vue {
 
   turnOffTheLights() {
     // Implement Me
+  }
+
+  purpleTheme() {
+    const purpleTheme = {
+      primary: colors.deepPurple,
+      accent: colors.purple.accent3,
+      secondary: colors.purple,
+      info: '#03A9F4',
+      warning: colors.pink.accent1,
+      error: colors.pink.accent3,
+      success: colors.deepPurple.lighten4,
+    }
+
+    this.$vuetify.theme.themes.dark = purpleTheme
+    this.$vuetify.theme.themes.light = purpleTheme
   }
 }
 </script>
