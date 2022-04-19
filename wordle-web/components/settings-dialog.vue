9f5c40c09c48877816a4ce9f83a01b1f7b0195c3
Meghan <Meg.Woodford@IntelliTect.com>
2022-04-19 15:35:11 -0700

    Lecture 4/19

---
+++ wordle-web/components/settings-dialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-btn icon @click="toggleDialog">
+      <v-icon> mdi-cog </v-icon>
+    </v-btn>
+
+    <v-dialog v-model="dialog" width="450">
+      <v-card>
+        <v-container>
+          <v-card-title> Settings </v-card-title>
+
+          <v-card-text>
+            <v-menu offset-y>
+              <template #activator="{ on, attrs }">
+                <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                  Pick Theme
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item-group>
+                  <v-list-item @click="turnOnTheLights">
+                    <v-list-item-title> Light Mode </v-list-item-title>
+                  </v-list-item>
+                  <v-list-item @click="turnOffTheLights">
+                    <v-list-item-title> Dark Mode </v-list-item-title>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-menu>
+          </v-card-text>
+        </v-container>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class SettingsDialog extends Vue {
+  dialog = false
+
+  toggleDialog() {
+    this.dialog = !this.dialog
+  }
+
+  turnOnTheLights() {
+    this.$vuetify.theme.dark = false
+  }
+
+  turnOffTheLights() {
+    // Implement Me
+  }
+}
+</script>
