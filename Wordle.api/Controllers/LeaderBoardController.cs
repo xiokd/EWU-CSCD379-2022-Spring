0c6b1e84b400562418ecb3f0d8cf5814ae27cdd0
Grant Erickson <grant@IntelliTect.com>
2022-04-21 15:59:44 -0700

    added Score and LeaderBoardController

---
+++ Wordle.api/Controllers/LeaderBoardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wordle.api.Services;
+
+namespace Wordle.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LeaderBoardController
+    {
+        [HttpGet]
+        public IEnumerable<Score> Get()
+        {
+            List<Score> results = new ()
+            {
+                new Score("Hildagaurd", 25, 2.6),
+                new Score("Ralph", 30, 3.4),
+                new Score("Gene", 50, 4.1),
+            };
+            return results;
+        }
+
+        
+
+    }
+}
