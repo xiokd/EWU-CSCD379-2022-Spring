20618aefcfbc15bf53788a5bbc5f5b420382070e
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-04-26 18:20:09 -0700

    Updated the app bar and added menu option for an about page

---
+++ wordle-web/components/menu-options.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <v-btn icon @click="rightDrawer = !rightDrawer"> 
+        <v-icon> mdi-menu </v-icon> 
+    </v-btn>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" app absolute temporary fixed>
+        <v-list>
+            <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+            >
+                <v-list-item-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title v-text="item.title" />
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class MenuOptions extends Vue{
+    data() {
+        return {
+            rightDrawer: false,
+            right: true,
+            items: [
+                {
+                    icon: 'mdi-information',
+                    title: 'About Us',
+                    to: '/about',
+                },
+            ]
+        }
+    }
+}
+</script>
