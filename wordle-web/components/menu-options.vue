41e9e375904ed82f99f585a0b44aed1da1d8ac1c
Keng Xiong <74529239+xiokd@users.noreply.github.com>
2022-04-26 20:05:03 -0700

    Revised menu options file

--- wordle-web/components/menu-options.vue
+++ wordle-web/components/menu-options.vue
@@ -36,7 +36,7 @@ export default class MenuOptions extends Vue{
             items: [
                 {
                     icon: 'mdi-information',
-                    title: 'About Us',
+                    title: 'About',
                     to: '/about',
                 },
             ]
