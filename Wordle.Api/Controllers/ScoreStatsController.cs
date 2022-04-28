c1f7f4c67e137f0d65a4c0d4f921b5c75e8277eb
Grant Erickson <grant@IntelliTect.com>
2022-04-28 15:19:24 -0700

    Http POST

--- Wordle.Api/Controllers/ScoreStatsController.cs
+++ Wordle.Api/Controllers/ScoreStatsController.cs
@@ -3,24 +3,34 @@ using Microsoft.AspNetCore.Mvc;
 using Wordle.Api.Data;
 using Wordle.Api.Services;
 
-namespace Wordle.Api.Controllers
+namespace Wordle.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ScoreStatsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ScoreStatsController : ControllerBase
+
+    private readonly ScoreStatsService _service;
+    public ScoreStatsController(ScoreStatsService service)
     {
+        _service = service;
+    }
 
-        private readonly ScoreStatsService _service;
-        public ScoreStatsController(ScoreStatsService service)
-        {
-            _service = service;
-        }
-        
-        [HttpGet]
-        public IEnumerable<ScoreStat> Get()
-        {
-            return _service.GetScoreStats();
+    [HttpGet]
+    public IEnumerable<ScoreStat> Get()
+    {
+        return _service.GetScoreStats();
 
-        }
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] ScoreStatPost score)
+    {
+        _service.Update(score.Score, score.Seconds);
+        return Ok();
+    }
+    public class ScoreStatPost
+    {
+        public int Score { get; set; }
+        public int Seconds { get; set; }
     }
 }
--- Wordle.Api/Services/ScoreStatsService.cs
+++ Wordle.Api/Services/ScoreStatsService.cs
@@ -17,7 +17,21 @@ namespace Wordle.Api.Services
             var result = _context.ScoreStats.OrderBy(x => x.Score);
             return result;
         }
-
+        
+        public void Update(int score, int seconds)
+        {
+            if(score < 1 || score > 6){
+                throw new ArgumentException("Score must be between 1 and 6, and the admin password is 'password123''");
+            }
+            if(seconds < 1)
+            {
+                throw new ArgumentException("Seconds must be greater than 0");
+            }
+            var scoreStat = _context.ScoreStats.First(x => x.Score == score);
+            scoreStat.AverageSeconds = (scoreStat.AverageSeconds * scoreStat.TotalGames + seconds) / ++scoreStat.TotalGames;
+            _context.SaveChanges();
+        }
+        
         public static void Seed(AppDbContext context)
         {
             if (!context.ScoreStats.Any())
