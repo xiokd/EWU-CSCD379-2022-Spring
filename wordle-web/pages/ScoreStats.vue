304e1d077500e4c0441de585e986f69b837dfd2b
Grant Erickson <grant@IntelliTect.com>
2022-05-05 16:07:31 -0700

    Display stats

--- wordle-web/pages/ScoreStats.vue
+++ wordle-web/pages/ScoreStats.vue
@@ -8,13 +8,30 @@
       </template>
       <span> Go Home </span>
     </v-tooltip>
-
-    <v-card-text class="text-h1 font-weight-black text-center">
-      Wordle Game Stats!
+    <v-card>
+        <v-card-title>
+          <h1 class="display-1">Score Stats</h1>
+        </v-card-title>
+    <v-card-text>
+        <v-simple-table>
+            <thead>
+                <tr>
+                    <th>Score</th>
+                    <th>Avg. Seconds</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(stat, index) in stats " :key="index">
+                    <td>{{ stat.score }}</td>
+                    <td>{{ stat.averageSeconds }}</td>
+                     </tr>
+            </tbody>
+        </v-simple-table>
     </v-card-text>
     <v-card-actions>
         <v-btn color="primary" @click="refreshStats"> Refresh </v-btn>
     </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
