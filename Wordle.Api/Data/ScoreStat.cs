74ab0b15a6e4d26e24bd889baffeddb69131769f
schuyler <sasplin@ewu.edu>
2022-05-10 17:17:48 -0700

    c# changes before changing repos

--- Wordle.Api/Data/ScoreStat.cs
+++ Wordle.Api/Data/ScoreStat.cs
@@ -10,6 +10,8 @@ namespace Wordle.Api.Data
         public int AverageSeconds { get; set; }
         public int TotalGames { get; set; }
 
+//   DTO: Data Transfer Object 
+// - keeps us from exposing and allowing bad data to be passed to the database.
         public ScoreStat Clone()
         {
             return new ScoreStat
